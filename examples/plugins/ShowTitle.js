5f082a40fd4dc00edc17aa04ef638dddac98d04b
lunafromthemoon <je.luna.mdq@gmail.com>
2021-09-07 16:47:30 +0200

    new GUI format for examples

--- examples/plugins/ShowTitle.js
+++ examples/plugins/ShowTitle.js
@@ -1,14 +1,14 @@
-class ShowTitle extends RenJS.Plugin {
+class SHOWTITLE extends RenJS.Plugin {
 
-	onCall(params) {
+    onCall(params) {
         // load 'title' background in extra section of setup!!!
-		const bg = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'title');
+        const bg = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'title');
         bg.alpha = 0;
         bg.anchor.set(0.5);
-        const style = {...this.game.gui.getTextStyle('choice')};
-        style.font = '50pt ' + this.game.gui.fonts[0];
+        const style = {...this.game.gui.hud.cHandlers.default.config.text.style};
+        style.fontSize = '50pt';
         const title = this.game.add.text(0, -20, params.title, style);
-        style.font = '25pt ' + this.game.gui.fonts[0];
+        style.fontSize = '25pt';
         const subtitle = this.game.add.text(0, 40, params.subtitle, style);
         subtitle.anchor.set(0.5);
         title.anchor.set(0.5);
@@ -23,7 +23,10 @@ class ShowTitle extends RenJS.Plugin {
                 }, delay: this.game.storyConfig.fadetime * 2
             }
         ], true, this.game.storyConfig.fadetime * 2);
-	}
+    }
 }
 
-RenJSGame.addPlugin('SHOWTITLE',ShowTitle);
+RenJSGame.addPlugin('SHOWTITLE',SHOWTITLE);
+
+
+
